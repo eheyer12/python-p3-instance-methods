feb77f3a404f625d2704c1c757fb854131b56ad5
Eduardo Heyer <eduardo.heyer12@gmail.com>
2023-06-06 10:04:13 -0400

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,16 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
+      
+        
+class Person:
+    
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
